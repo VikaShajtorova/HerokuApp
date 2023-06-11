5bc0f809ec78d6b460325deae10c57ca429a8f2a
Victoria Shaitorova <shajtorova@mail.ru>
2023-06-11 19:35:50 +0300

    Initial commit

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.List;
+
+public class AddRemoveElementsTest extends BaseTest{
+    @Test
+    public void addRemoveElements(){
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addElementButton = driver.findElement(By.cssSelector("[onclick='addElement()']"));
+        addElementButton.click();
+        addElementButton.click();
+
+
+        List<WebElement> deleteButtonsList = driver.findElements(By.cssSelector("[onclick='deleteElement()']"));
+        assertEquals(deleteButtonsList.size(),2,"На странице не 2 элемента");
+
+        deleteButtonsList.get(0).click();
+        deleteButtonsList = driver.findElements(By.cssSelector("[onclick='deleteElement()']"));
+        assertEquals(deleteButtonsList.size(),1,"На странице не 2 элемента");
+
+
+    }
+
+
+}
---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+
+public class CheckboxesTest extends BaseTest {
+
+    @Test
+    public void checkCheckboxes() {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("[type='checkbox']"));
+        assertFalse(checkBoxes.get(0).isSelected(), "Чек-бокс включен");
+        checkBoxes.get(0).click();
+        assertTrue(checkBoxes.get(0).isSelected(), "Чек-бокс выключен");
+
+        assertTrue(checkBoxes.get(1).isSelected(), "Чек-бокс включен");
+        checkBoxes.get(1).click();
+        assertFalse(checkBoxes.get(1).isSelected(), "Чек-бокс выключен");
+
+
+    }
+}
